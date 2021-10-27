2cc637997d524eca535780ca6a2dd6a767f87b37
bbrunoo <brunoedsanntos@gmail.com>
2021-10-27 19:15:54 -0300

    atv

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+namespace tarefa2710
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tabuada =  new string[10];
+        
+            //WriteLine($"Novo valor no index 2: {nomes[2]}");
+                WriteLine("Digite o numero da tabuada");
+            double num = Convert.ToInt32(ReadLine());
+             
+
+            for (int i = 1; i <= 10; i++)
+            {
+                 double r = num * i; 
+                 WriteLine(num + "x" +  i + "=" + r );
+            }   
+        }
+    }
+}
+
